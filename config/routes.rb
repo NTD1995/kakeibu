9f78b4507efc07f6ba67e070dcbe67c331af5bb1
abedaiki <abe12261994@gmail.com>
2025-03-01 17:56:04 +0000

    Fix topページ、ヘッダー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
- root to: "homes#top"
+  # ユーザー側
+  # トップページ
+  root to: "public/homes#top"
+
+  # 管理者側
+  # 管理者トップページ（投稿一覧）
+  get "/admin", to: "admin/homes#top", as: "admin/top"
 end
