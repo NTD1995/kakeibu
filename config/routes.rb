750c9f014baa18a2ef2546b38031b3d35649b618
abedaiki <abe12261994@gmail.com>
2025-03-01 18:23:54 +0000

    Add aboutページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # ユーザー側
   # トップページ
   root to: "public/homes#top"
+  # アバウトページ
+  get "/about", to: "public/homes#about", as: "about"
 
   # 管理者側
   # 管理者トップページ（投稿一覧）
