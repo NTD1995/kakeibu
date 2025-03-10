e8408e0bc6817bba2b2f966928793ed1705d8ea2
abedaiki <abe12261994@gmail.com>
2025-03-10 07:55:05 +0000

    Add コメント機能の非同期通信

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -4,14 +4,12 @@ class Public::PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_id = post_comment.id
     comment.save
-    redirect_to post_path(post_comment)
   end
 
   def destroy
     post = Post.find(params[:post_id])
     comment = current_user.post_comments.find_by(post_id: post.id)
     comment.destroy
-    redirect_to post_path(post)
   end
 
   private
