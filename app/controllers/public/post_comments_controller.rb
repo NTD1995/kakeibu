f152836857f43806fcdec8e1086a6f4fe0f68408
abedaiki <abe12261994@gmail.com>
2025-03-23 08:41:03 +0000

    Update ユーザー側のコメントのフラッシュメッセージとエラーメッセージの修正

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -6,7 +6,11 @@ class Public::PostCommentsController < ApplicationController
     post_comment = Post.find(params[:post_id])
     @comment = current_user.post_comments.new(post_comment_params)
     @comment.post_id = post_comment.id
-    @comment.save
+    if @comment.save
+      flash.now[:notice] = "コメントの投稿に成功しました。"
+    else
+      flash.now[:alert] = "コメントの投稿に失敗しました。"
+    end
   end
 
   # コメント削除処理
@@ -14,6 +18,7 @@ class Public::PostCommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = PostComment.find(params[:id])
     @comment.destroy
+    flash.now[:alert] = "コメントを削除しました。"
   end
 
   private
